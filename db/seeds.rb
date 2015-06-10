e99df258b4ca770acfd874872f6be8341987c4d6
tzsiga <tzsiga@gmail.com>
2015-06-10 10:21:05 +0200

    Seed FTW

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+propertyList = [
+  ["Jedi Temple", 800, "recreation centre, hangar", "academy", 5, "Coruscant"],
+  ["Death Star", 10000, "throne room, super laser, hangar", "space station", 5, "in a galaxy, far far away"],
+  ["Star Labs", 130, "particle accelerator, secret room", "mansion", 4, "Central City, Downtown str. 28."],
+  ["Batcave", 1, "garage, computer room, armory", "cave", 5, "under the Wayne mansion"],
+  ["Coppola Estate", 66, "armory, kitchen, garage, swimming pool", "mansion", 5, "321 Little Italy, Via Appia"],
+]
+
+propertyList.each do |name, rooms, facilities, category, rating, address|
+  Property.create(
+    name: name,
+    rooms: rooms,
+    facilities: facilities,
+    category: category,
+    rating: rating,
+    address: address
+  )
+end
+
+userList = [
+  ["guest@user.com", "Guest", "User"],
+  ["test@user.com", "Test", "User"],
+  ["test@admin.com", "Test", "Admin"]
+]
+
+userList.each do |email, first_name, last_name|
+  User.create(
+    email: email,
+    first_name: first_name,
+    last_name: last_name
+  )
+end
