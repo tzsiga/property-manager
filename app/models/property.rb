0536104187497a20b82ca30c06856a504749ea6f
tzsiga <tzsiga@gmail.com>
2015-06-10 14:23:25 +0200

    View fixes

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,9 +1,9 @@
 class Property < ActiveRecord::Base
   has_many :reservations
-  validates :name, presence: true
-  validates :rooms, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :rooms, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :facilities, presence: true
   validates :category, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
   validates :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
   has_many :reservations
-  validates :email, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, email: true
 end
