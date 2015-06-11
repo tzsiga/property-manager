cc1844c4c5137cb7302207ea0050c60202605e76
tzsiga <tzsiga@gmail.com>
2015-06-12 00:55:55 +0200

    Datetime fix in migration

--- db/migrate/20150609123551_create_reservations.rb
+++ db/migrate/20150609123551_create_reservations.rb
@@ -2,10 +2,10 @@ class CreateReservations < ActiveRecord::Migration
   def change
     create_table :reservations do |t|
       t.integer :property_id, index: true, foreign_key: true, null: false
-      t.timestamp :from, null: false
-      t.timestamp :to, null: false
+      t.datetime :from, null: false
+      t.datetime :to, null: false
       t.integer :user_id, index: true, foreign_key: true, null: false
-      t.timestamp null: false
+      t.datetime null: false
     end
   end
 end
