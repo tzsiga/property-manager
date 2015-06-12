373f9cd3b4da20fd0eda3bd554ed0208143bbc6d
tzsiga <tzsiga@gmail.com>
2015-06-12 07:51:57 +0200

    Timezone fix

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module PropertyManager
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.time_zone = 'Budapest'
-    
+    config.time_zone = 'Europe/Budapest'
+    config.active_record.default_timezone = :local
+
   end
 end
