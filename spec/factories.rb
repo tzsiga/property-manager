2295eb83e81b0c241c056c54d48a4f21e0a090f7
tzsiga <tzsiga@gmail.com>
2015-06-16 19:55:42 +0200

    Factory test

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 FactoryGirl.define do
   factory :user do
-    first_name "Factory"
-    last_name  "User"
-    email "user@factory.com"
+    first_name 'Factory'
+    last_name  'User'
+    email 'user@factory.com'
   end
 end
--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -6,6 +6,10 @@ describe 'Users' do
 
     user = build(:user)
 
+    expect(user.first_name).to eq 'Factory'
+    expect(user.last_name).to eq 'User'
+    expect(user.email).to eq 'user@factory.com'
+
     expect(page).to have_css '.btn-info', 'New User'
   end
 end
